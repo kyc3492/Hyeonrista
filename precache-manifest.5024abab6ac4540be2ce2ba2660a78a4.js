ad8e2f0ab5149db58da4381c1d4aa7ec6413a621
강연철 <gang-yeoncheol@gang-yeoncheol-ui-MacBook-Pro.local>
2022-05-06 00:49:04 +0900

    Updates

---
+++ precache-manifest.5024abab6ac4540be2ce2ba2660a78a4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "83a52084266f26a7035f",
+    "url": "/static/css/main.29b08ffa.chunk.css"
+  },
+  {
+    "revision": "83a52084266f26a7035f",
+    "url": "/static/js/main.83a52084.chunk.js"
+  },
+  {
+    "revision": "8139a818b7d886f0e518",
+    "url": "/static/css/1.d4569296.chunk.css"
+  },
+  {
+    "revision": "8139a818b7d886f0e518",
+    "url": "/static/js/1.8139a818.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d47658521d062fe3e4eeabadcc106a58",
+    "url": "/static/media/cafe3.d4765852.jpeg"
+  },
+  {
+    "revision": "6071c11cef4e5ad308942fbc0d5a60b1",
+    "url": "/static/media/cafe4.6071c11c.jpeg"
+  },
+  {
+    "revision": "34910b1a92bf79bd4661691ad51bf12d",
+    "url": "/static/media/cafe2.34910b1a.jpeg"
+  },
+  {
+    "revision": "fdf6c5b74e39ec878350acd6a95aff19",
+    "url": "/static/media/cafe1.fdf6c5b7.png"
+  },
+  {
+    "revision": "0c0a47e2e29f06a31c3809ead0c74cbc",
+    "url": "/static/media/developing_background.0c0a47e2.jpg"
+  },
+  {
+    "revision": "b8803abf3a3c61d34982f55b27a5d846",
+    "url": "/static/media/body_background.b8803abf.jpg"
+  },
+  {
+    "revision": "a28ed093d6748a8be80913ab577c3bcc",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Hyeonrista/precache-manifest.38a7a40ac52ac2218d770dc83a3661ce.js"
+  "/precache-manifest.5024abab6ac4540be2ce2ba2660a78a4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Hyeonrista/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
